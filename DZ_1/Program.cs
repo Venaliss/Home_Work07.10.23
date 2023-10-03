9465a63dfa6279634902bbe8c50f7363a2d22648
Venaliss <venaliss2023@yandex.ru>
2023-10-03 22:15:05 +0300

    Тумаков 5.1

--- DZ_1/Program.cs
+++ DZ_1/Program.cs
@@ -7,9 +7,23 @@ using System.Threading.Tasks;
 namespace DZ_1
 {
     class Program
-    {
+    {   /*Создать метод который возвращает максимальное из двух значений*/
+        static long Max(long Number1, long Number2 )
+        {
+            return Number1 > Number2 ? Number1 : Number2;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Упражнение 5.1 Тумаков - Создать метод который возвращает максимальное из двух введеных чисел\n");
+            Console.WriteLine("Введите два числовых значения:");
+            if (Int64.TryParse(Console.ReadLine(), out long Number1) && Int64.TryParse(Console.ReadLine(), out long Number2))
+            {
+                Console.WriteLine($"Ваше максимальное число:{Max(Number1, Number2)}");
+            }
+            else
+            {
+                Console.WriteLine("Неправильный формат данных. Введите числовое значение.");
+            }
         }
     }
 }
